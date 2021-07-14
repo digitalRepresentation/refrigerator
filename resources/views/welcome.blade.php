b5cc3e392ffda1c1e0ea15824f6c3481a2baf315
digitalRepresentation <kinemura@naver.com>
2021-07-14 22:14:38 +0900

    reset common

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,8 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-        <link href=
+        
+        <link rel="stylesheet" type="text/css" href="/css/reset.css">
         
     </head>
         <header>
