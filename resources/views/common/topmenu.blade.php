947872514671222bebdb024c75feaa7deea1dde7
digitalRepresentation <kinemura@naver.com>
2021-07-18 21:14:29 +0900

    login 追加

--- resources/views/common/topmenu.blade.php
+++ resources/views/common/topmenu.blade.php
@@ -13,12 +13,17 @@
 </head>
 <body>
     <header>
+            <div class="login">
+                <a href="/login">login</a>
+            </div>
+            <!-- menu -->
             <div class="topnav">
                 <a class="active" href="#home">Home</a>
                 <a href="#news">News</a>
                 <a href="#contact">Contact</a>
                 <a href="#about">About</a>
             </div>
+            
             @yield('header')
     </header>
     <article>
