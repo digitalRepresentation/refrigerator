e3c7eaadcda9713be3bab085b9c34340669a38e5
digitalRepresentation <kinemura@naver.com>
2021-07-18 17:43:15 +0900

    blade 活用

--- resources/views/common/topmenu.blade.php
+++ resources/views/common/topmenu.blade.php
@@ -4,22 +4,28 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    @yield('head')
         <!-- cssReset -->
         <link rel="stylesheet" href="{{asset('css/common/reset.css')}}" />
         <!-- cssMenu -->
         <link rel="stylesheet" href="{{asset('css/common/menuTop.css')}}" />
+        @yield('style')
 </head>
 <body>
     <header>
-        @section('sidebar')
             <div class="topnav">
                 <a class="active" href="#home">Home</a>
                 <a href="#news">News</a>
                 <a href="#contact">Contact</a>
                 <a href="#about">About</a>
             </div>
-        @show    
+            @yield('header')
     </header>
+    <article>
+        @yield('content')
+    </article>
+    <footer>
+        @yield('footer')
+    </footer>
 </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,36 +1,5 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>冷蔵高管理システム</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-        <!-- cssReset -->
-        <link rel="stylesheet" href="{{asset('css/common/reset.css')}}" />
-        <!-- cssMenu -->
-        <link rel="stylesheet" href="{{asset('css/common/menuTop.css')}}" />
-    </head>
-        <header>
-            <div class="topnav">
-                <a class="active" href="#home">Home</a>
-                <a href="#news">News</a>
-                <a href="#contact">Contact</a>
-                <a href="#about">About</a>
-            </div>
-            
-        </header>
-        <article></article>
-        <footer></footer>
-    </body>
-</html>
-
 @extends('common.topmenu')
 
-@section('sidebar')
-    @parent
-
-    <p>This is appended to the master sidebar.</p>
+@section('head')
+    <title>冷蔵庫管理システム</title>
 @endsection
