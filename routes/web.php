9b4418a43fee3f00508d012ceb4e147efb9a133a
digitalRepresentation <kinemura@naver.com>
2021-07-19 21:23:40 +0900

    login get route add

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,13 +18,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-//Auth::routes();
+//ログイン画面
+Route::get('/login', [LoginController::class, 'index']);
 
+//Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-//Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-//
-Route::post('/login', 'LoginController@index');
