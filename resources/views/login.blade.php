55a0603469350b5597def44fad3eb07677cdcaaf
digitalRepresentation <kinemura@naver.com>
2021-07-20 22:07:26 +0900

    loginForm css import

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,27 +4,31 @@
     <title>冷蔵庫管理システム</title>
 @endsection
 
+@section('style')
+    <!-- cssMenu -->
+    <link rel="stylesheet" href="{{asset('css/loginForm.css')}}" />
+@endsection
+
 @section('content')
-<form>
-    <div class="panel panel-success">
-        <div class="panel-heading">
-        Panel content
+    <form class="loginForm">
+        <div class="panel panel-success">
+            <div class="panel-heading">
+            ログインページ
+            </div>
+            <div class="panel-title">
+                <div class="form-group">
+                    <input type="text" class="form-control" placeholder="ID">
+                </div>
+                <div class="form-group">
+                    <input type="password" class="form-control" placeholder="パスワード">
+                </div>
+                <div class="checkbox">
+                    <label>
+                        <input type="checkbox"> 入力情報を記憶します。
+                    </label>
+                </div>
+                <button type="submit" class="btn btn-success">ログイン</button>
+            </div>
         </div>
-        <div class="panel-title">
-            <div class="form-group">
-                <input type="text" class="form-control" placeholder="ID">
-            </div>
-            <div class="form-group">
-                <input type="password" class="form-control" placeholder="パスワード">
-            </div>
-            <div class="checkbox">
-                <label>
-                    <input type="checkbox"> 入力情報を記憶します。
-                </label>
-            </div>
-            <button type="submit" class="btn btn-default">ログイン</button>
-        </div>
-    </div>
-    
-</form>
+    </form>
 @endsection
