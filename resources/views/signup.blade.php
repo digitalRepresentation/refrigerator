d8d6f44935d160d262d677604e062d979a15f010
digitalRepresentation <kinemura@naver.com>
2021-07-22 16:38:06 +0900

    signup route 連携

---
+++ resources/views/signup.blade.php
@@ -0,0 +1,14 @@
+@extends('common.topmenu')
+
+@section('head')
+    <title>冷蔵庫管理システム</title>
+@endsection
+
+@section('style')
+    <!-- cssMenu -->
+    <link rel="stylesheet" href="{{asset('css/loginForm.css')}}" />
+@endsection
+
+@section('content')
+    <h1>asd</h1>
+@endsection
