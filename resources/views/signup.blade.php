2d3a96b835680afe8add670010c72c2893b13a6f
digitalRepresentation <kinemura@naver.com>
2021-07-30 22:31:27 +0900

    signup name add

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -11,6 +11,19 @@
   <div class="card-body">
     <form method="POST" action="{{ route('signup') }}">
       @csrf
+
+      <div class="form-group row">
+          <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+          <div class="col-md-6">
+              <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+              @error('name')
+                  <span class="invalid-feedback" role="alert">
+                      <strong>{{ $message }}</strong>
+                  </span>
+              @enderror
+          </div>
+      </div>
+
       <div class="form-row">
         <div class="form-group col-md-6">
           <label for="inputEmail4">Email</label>
