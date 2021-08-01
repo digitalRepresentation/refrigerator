047be584f0dea0b58f385342109786e48009f341
digitalRepresentation <kinemura@naver.com>
2021-08-01 22:57:59 +0900

    member model data 挿入して

--- app/Models/Member.php
+++ app/Models/Member.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Member extends Model
 {
     use HasFactory;
+
+    //table名
+    protected $table = 'Member';
+    //parimaryキー
+    protected $primaryKey = 'MEMBER_NAME';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+
+    protected $fillable = [
+        'MEMBER_NAME',
+        'MEMBER_EMAIL',
+        'MEMBER_PASSWORD',
+        'MEMBER_ADDRESS' 
+    ];
 }
