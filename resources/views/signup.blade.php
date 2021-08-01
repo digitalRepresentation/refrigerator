278a45cef50e3e859799a3bde0176bc5baee648c
digitalRepresentation <kinemura@naver.com>
2021-08-01 20:20:09 +0900

    email, password value　追加して

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -18,7 +18,7 @@
               <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
               @error('name')
                   <span class="invalid-feedback" role="alert">
-                      <strong>{{ $message }}</strong>
+                      <strong>{{ $message }} </strong>
                   </span>
               @enderror
           </div>
@@ -27,16 +27,16 @@
       <div class="form-row">
         <div class="form-group col-md-6">
           <label for="inputEmail4">Email</label>
-          <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
+          <input type="email" class="form-control" value="{{ old('email') }}" id="inputEmail4" placeholder="Email">
         </div>
         <div class="form-group col-md-6">
-          <label for="inputPassword4">Password</label>
-          <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
+          <label for="inputPassword4">パスワード</label>
+          <input type="password" class="form-control" value="{{ old('password') }}" id="inputPassword4" placeholder="Password">
         </div>
       </div>
       <div class="form-group">
-        <label for="inputAddress">Address</label>
-        <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+        <label for="inputAddress">住所</label>
+        <input type="text" class="form-control" value="{{ old('address') }}"   id="inputAddress" placeholder="1234 Main St">
       </div>
       <div class="form-group">
         <label for="inputAddress2">Address 2</label>
