86c7bea5a885763878157689ed1e501c49653943
digitalRepresentation <kinemura@naver.com>
2021-08-07 13:30:22 +0900

    Validation Check

--- app/Http/Controllers/SignUpController.php
+++ app/Http/Controllers/SignUpController.php
@@ -41,6 +41,15 @@ class SignUpController extends Controller
      * @var Request $request
      */
     public function signupCreate(Request $request) {
+
+        //Validation Check
+        $this->validate($request, [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'address' => ['required', 'string', 'min:8', 'confirmed'],
+        ]);
+
         $name = $request->input('name');
         $email = $request->input('email');
         $password = $request->input('password');
@@ -48,10 +57,10 @@ class SignUpController extends Controller
         
         
         $signupData = array(
-             "name" => $name,
-             "email" => $email,
-             "password" => $password,
-             "address" => $address,
+            "name" => $name,
+            "email" => $email,
+            "password" => $password,
+            "address" => $address,
          );
         
         $validatorCheck = $this->validator($signupData);
@@ -62,64 +71,64 @@ class SignUpController extends Controller
         return view('welcome');
     }
 
-    /**
-     * data insert処理
-     *
-     * @var array $signupData
-     */
-    public function signupDataInsert(array $signupData) {
-        try {
-        //memberテーブル作成
-        DB::table('member')->insert([
-            [
-                'MEMBER_NAME' => $signupData["name"], 
-                'MEMBER_PASSWORD' => Hash::make($signupData["password"]), 
-                'MEMBER_ADDRESS' => $signupData["address"], 
-                'MEMBER_EMAIL' => $signupData["email"]
-            ]
-        ]);
-        } catch (\Exception $e) {
-            report($e);
-            echo 'Connection failed: ' . $e->getMessage();
-        }
+    // /**
+    //  * data insert処理
+    //  *
+    //  * @var array $signupData
+    //  */
+    // public function signupDataInsert(array $signupData) {
+    //     try {
+    //     //memberテーブル作成
+    //     DB::table('member')->insert([
+    //         [
+    //             'MEMBER_NAME' => $signupData["name"], 
+    //             'MEMBER_PASSWORD' => Hash::make($signupData["password"]), 
+    //             'MEMBER_ADDRESS' => $signupData["address"], 
+    //             'MEMBER_EMAIL' => $signupData["email"]
+    //         ]
+    //     ]);
+    //     } catch (\Exception $e) {
+    //         report($e);
+    //         echo 'Connection failed: ' . $e->getMessage();
+    //     }
         
-    }
-
-    // public function __construct()
-    // {
-    //     $this->middlewre('guest');
     // }
 
-    /**
-     * Get a validator for an incoming registration request.
-     *
-     * @param  array  $data
-     * @return \Illuminate\Contracts\Validation\Validator
-     */
-    protected function validator(array $signupData)
-    {
-        return Validator::make($signupData, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
-            'address' => ['required', 'string', 'min:8', 'confirmed'],
-        ]);
-    }
+    // // public function __construct()
+    // // {
+    // //     $this->middlewre('guest');
+    // // }
 
-    /**
-     * Create a new user instance after a valid registration.
-     *
-     * @param  array  $data
-     * @return \App\Models\User
-     */
-    protected function create(array $data)
-    {
+    // /**
+    //  * Get a validator for an incoming registration request.
+    //  *
+    //  * @param  array  $data
+    //  * @return \Illuminate\Contracts\Validation\Validator
+    //  */
+    // protected function validator(array $signupData)
+    // {
+    //     return Validator::make($signupData, [
+    //         'name' => ['required', 'string', 'max:255'],
+    //         'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+    //         'password' => ['required', 'string', 'min:8', 'confirmed'],
+    //         'address' => ['required', 'string', 'min:8', 'confirmed'],
+    //     ]);
+    // }
+
+    // /**
+    //  * Create a new user instance after a valid registration.
+    //  *
+    //  * @param  array  $data
+    //  * @return \App\Models\User
+    //  */
+    // protected function create(array $data)
+    // {
 
 
-        return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => Hash::make($data['password']),
-        ]);
-    }
+    //     return User::create([
+    //         'name' => $data['name'],
+    //         'email' => $data['email'],
+    //         'password' => Hash::make($data['password']),
+    //     ]);
+    // }
 }
