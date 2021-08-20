722eb5889532a0f348312451d11577f7ae6e2af1
digitalRepresentation <kinemura@naver.com>
2021-08-21 08:11:57 +0900

    いらないdiv消して

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -39,18 +39,6 @@
           @enderror
         </div>
       </div>
-
-      <!-- <div class="form-row">
-        <div class="form-group col-md-6">
-          <label for="inputPassword4">パスワード確認</label>
-          <input type="password" class="form-control @error('password') is-invalid @enderror" value="{{ old('password') }}" id="password" autocomplete="password" name="password" placeholder="Password">
-          @error('password')
-                  <span class="invalid-feedback" role="alert">
-                      <strong>{{ $message }} </strong>
-                  </span>
-          @enderror
-        </div>
-      </div> -->
       
       <div class="form-row">
         <div class="form-group col-md-6">
@@ -74,27 +62,6 @@
                   </span>
         @enderror
       </div>
-      <!-- <div class="form-group">
-        <label for="inputAddress2">Address 2</label>
-        <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
-      </div>
-      <div class="form-row">
-        <div class="form-group col-md-6">
-          <label for="inputCity">City</label>
-          <input type="text" class="form-control" id="inputCity">
-        </div>
-        <div class="form-group col-md-4">
-          <label for="inputState">State</label>
-          <select id="inputState" class="form-control">
-            <option selected>Choose...</option>
-            <option>...</option>
-          </select>
-        </div>
-        <div class="form-group col-md-2">
-          <label for="inputZip">Zip</label>
-          <input type="text" class="form-control" id="inputZip">
-        </div>
-      </div> -->
       <div class="form-group">
         <div class="form-check">
           <input class="form-check-input" type="checkbox" id="gridCheck">
