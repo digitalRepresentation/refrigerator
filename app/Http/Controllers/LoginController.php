550ad54d8e607d5424c36a868d5ac150d439aa69
digitalRepresentation <kinemura@naver.com>
2021-08-23 21:58:33 +0900

    login 失敗処理

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,10 +3,37 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+//認証
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
     public function index() {
         return view('login');
+    }
+
+        /**
+     * SignUpの引数を取得するメソッド
+     *
+     * @var Request $request
+     */
+    public function customLogin(Request $request) {
+
+        //Validation Check
+        $this->validate($request, [
+            'name' => ['required'],
+            'password' => ['required'],
+        ]);
+
+        $credentials = $request->only('name', 'password');
+
+        if (Auth::attempt($credentials)) {
+            echo "true";
+            exit;
+            return redirect()->intended('dashboard')
+                        ->withSuccess('Signed in');
+        }
+  
+        return redirect("login")->withSuccess('Login details are not valid');
     }
 }
