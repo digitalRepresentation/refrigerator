a00c805ccaf5b069592ce2579c2b02a7216864da
digitalRepresentation <kinemura@naver.com>
2021-08-25 23:52:26 +0900

    login success logic

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 //認証
 use Illuminate\Support\Facades\Auth;
+//exception
+use Exception;
+use Illuminate\Support\Facades\Hash;
 
 class LoginController extends Controller
 {
@@ -18,22 +21,30 @@ class LoginController extends Controller
      * @var Request $request
      */
     public function customLogin(Request $request) {
-
         //Validation Check
+        try{    
+            // your code here.    
+        
         $this->validate($request, [
             'name' => ['required'],
             'password' => ['required'],
         ]);
 
-        $credentials = $request->only('name', 'password');
+        //$credentials = $request->only('ㅜ', 'password');
 
-        if (Auth::attempt($credentials)) {
+        
+
+        if (Auth::attempt(['MEMBER_NAME'=>$request->input('name'),'MEMBER_PASSWORD'=>Hash::check($request->input('password'))])) {
             echo "true";
             exit;
             return redirect()->intended('dashboard')
                         ->withSuccess('Signed in');
         }
-  
+        echo "false";
+        exit;
         return redirect("login")->withSuccess('Login details are not valid');
+        }catch(Exception $e){    
+            dd($e);    
+        }
     }
 }
