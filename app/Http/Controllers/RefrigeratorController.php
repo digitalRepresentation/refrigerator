29e9539b57cf82a234e8538855d6a25423534e27
digitalRepresentation <kinemura@naver.com>
2021-09-03 21:58:44 +0900

    RefrigeratorController addして

---
+++ app/Http/Controllers/RefrigeratorController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Refrigerator;
+use Illuminate\Http\Request;
+
+class RefrigeratorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Refrigerator  $refrigerator
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Refrigerator $refrigerator)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Refrigerator  $refrigerator
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Refrigerator $refrigerator)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Refrigerator  $refrigerator
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Refrigerator $refrigerator)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Refrigerator  $refrigerator
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Refrigerator $refrigerator)
+    {
+        //
+    }
+}
